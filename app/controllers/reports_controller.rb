3dd2e68b5920fe54d0f7a020cc33ada5150da25f
Alexander J. Alifimoff <aalifimoff@stanford.edu>
2015-05-08 16:04:27 -0700

    bugfixes...

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,7 +10,7 @@ class ReportsController < ApplicationController
         @report = Report.new
     end
     def create
-        stock = Stock.find_by_ticker params[:report][:stock_ticker]
+        stock = Stock.find_by_id params[:report][:stock_id]
         @report = Report.new(report_params(params[:report]))
         if stock && @report.save
             stock.reports << @report
