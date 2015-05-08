c90ef7cb61d44f4afec68f189b45fdc371d64a79
Alexander J. Alifimoff <aalifimoff@stanford.edu>
2015-05-07 17:46:10 -0700

    added transaction adding support

---
+++ app/controllers/manage_controller.rb
@@ -0,0 +1,19 @@
+class ManageController < ApplicationController
+    def add_transaction
+        @trade = Trade.new
+    end
+    
+    def create
+        @trade = Trade.new(trade_params(params[:trade]))
+        if @trade.save then
+            redirect_to 'display/log'
+        else
+            render 'manage/add_transaction'
+        end
+    end
+    
+    private
+    def trade_params(params)
+        return params.permit(:asset_ticker, :execution_date, :price, :quantity, :fees)
+    end
+end
